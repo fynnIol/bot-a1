ddbfb0c36c30bfdf1093122d0c656b54d810661a
fynnIol <schoolschool12346@gmail.com>
2026-04-13 18:22:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,21 @@ const client = new Client({
 });
 
 const CHANNEL_ID = "1491809073300242654";
-let latestJobId = null;
+let latestJob = null;
 
 client.on("messageCreate", (message) => {
   if (message.channel.id !== CHANNEL_ID) return;
-  const match = message.content.match(/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/);
-  if (match) latestJobId = match[0];
+  const match = message.content.match(/placeId=(\d+)&gameInstanceId=([\w-]+)/);
+  if (match) {
+    latestJob = {
+      placeId: match[1],
+      jobId: match[2]
+    };
+  }
 });
 
 app.get("/job", (req, res) => {
-  res.json({ jobId: latestJobId });
+  res.json(latestJob || {});
 });
 
 app.listen(3000, () => console.log("Bot is running!"));
