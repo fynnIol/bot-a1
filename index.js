0619033574309e2071dd3e05c725e70280eef5eb
fynnIol <schoolschool12346@gmail.com>
2026-04-13 18:35:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const express = require("express");
+const https = require("https");
 
 const app = express();
 const client = new Client({
@@ -15,7 +16,23 @@ let latestJob = null;
 
 client.on("messageCreate", (message) => {
   if (message.channel.id !== CHANNEL_ID) return;
-  const match = message.content.match(/placeId=(\d+)&gameInstanceId=([\w-]+)/);
+
+  // Check plain text content
+  let text = message.content;
+
+  // Also check embeds
+  if (message.embeds.length > 0) {
+    message.embeds.forEach(embed => {
+      if (embed.description) text += " " + embed.description;
+      if (embed.fields) {
+        embed.fields.forEach(field => {
+          text += " " + field.value;
+        });
+      }
+    });
+  }
+
+  const match = text.match(/placeId=(\d+)&gameInstanceId=([\w-]+)/);
   if (match) {
     latestJob = {
       placeId: match[1],
@@ -27,6 +44,14 @@ client.on("messageCreate", (message) => {
 app.get("/job", (req, res) => {
   res.json(latestJob || {});
 });
+
+setInterval(() => {
+  https.get("https://bot-a1.onrender.com/job", (res) => {
+    console.log("Kept alive:", res.statusCode);
+  }).on("error", (e) => {
+    console.error("Keep alive error:", e);
+  });
+}, 300000);
 
 app.listen(3000, () => console.log("Bot is running!"));
 client.login(process.env.BOT_TOKEN);
